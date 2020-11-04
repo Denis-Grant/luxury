3dc6b698a5d5159685980dcbb0054997f6717a4b
Apple <apple@MacbookPro.local>
2020-11-04 07:48:15 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        lightblack: "#2B302C"
+      },
+      width: {
+        twentyrem: "20rem"
+      },
+      height: {
+        third: "32rem"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
